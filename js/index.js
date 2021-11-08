5ad07c09b9d8736b33e89b711c6a0de05f087bcf
Sergio E. Prellezo <sereprec@gmail.com>
2021-11-08 10:14:10 -0300

    Cambio validación form

--- js/index.js
+++ js/index.js
@@ -1,33 +1,27 @@
-$('.nav-link').on('click',function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+$(".nav-link").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
+});
 
-
-$("#send").click(function(event) {
+$("#send").click(function (event) {
   let name = $("#name").val();
   let email = $("#email").val();
   let mensaje = $("#mensaje").val();
 
-  if(name != "" & email != "" && mensaje != "") {
-    $("#form")[0].reset(); 
-    $( "#msg" ).addClass( "status-msg").text('Mensaje enviado correctamente');
-    $( "#msg" ).removeClass( "status-disabled");
+  if ($("#form")[0].checkValidity()) {
+    $("#form")[0].reset();
+    $("#msg").addClass("status-msg").text("Mensaje enviado correctamente ✔");
+    $("#msg").removeClass("status-disabled");
     setTimeout(() => {
-      $( "#msg" ).removeClass( "status-msg");
-    $( "#msg" ).addClass( "status-disabled");
+      $("#msg").removeClass("status-msg");
+      $("#msg").addClass("status-disabled");
     }, 3000);
-   
-  }else{
-    console.log("incompleto")
-    $( "#msg" ).addClass( "status-msg").text('Todos los campos son obligatorios');
-    $( "#msg" ).removeClass( "status-disabled");
+  } else {
+    console.log("incompleto");
+    $("#msg").addClass("status-msg").text("Todos los campos son obligatorios");
+    $("#msg").removeClass("status-disabled");
     setTimeout(() => {
-      $( "#msg" ).removeClass( "status-msg");
-    $( "#msg" ).addClass( "status-disabled");
+      $("#msg").removeClass("status-msg");
+      $("#msg").addClass("status-disabled");
     }, 3000);
-
   }
-  
-  
 });
-
